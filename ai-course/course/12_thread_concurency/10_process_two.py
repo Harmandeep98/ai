333587b0a087e7d35db920e73ca754f1689c8ecb
Harmandeep Singh <115776636+Harmandeep98@users.noreply.github.com>
2026-01-06 11:50:36 +0530

    Update 10_process_two.py

--- ai-course/course/12_thread_concurency/10_process_two.py
+++ ai-course/course/12_thread_concurency/10_process_two.py
@@ -4,7 +4,7 @@ import time
 def cpu_heavy():
     print(f"Crunching some numbers...")
     total = 0
-    for i in range(10**9):
+    for i in range(10** 9):
         total += i
     print("DONE ✅")
 
